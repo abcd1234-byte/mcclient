29083114cc0eb6881e9de49d74dfa8176a35c29e
Thibaut Girka <thib@sitedethib.com>
2011-05-13 13:54:37 +0200

    Copyright + license header

--- extensions/octree.h
+++ extensions/octree.h
@@ -1,3 +1,16 @@
+/*
+ * Copyright (C) 2011 Thibaut GIRKA <thib@sitedethib.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; version 3 only.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #ifndef __OCTREE_H__
 #define __OCTREE_H__
 
--- extensions/world_renderer.c
+++ extensions/world_renderer.c
@@ -1,3 +1,16 @@
+/*
+ * Copyright (C) 2011 Thibaut GIRKA <thib@sitedethib.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; version 3 only.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #include "world_renderer.h"
 #include "sector.h"
 #include "blocktypes.h"
