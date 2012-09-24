cd49a0e3930a6b0044e20eac70ab3c5d455f4c66
Thibaut Girka <thib@sitedethib.com>
2012-09-24 21:12:26 +0200

    Refactor

--- mcclient.py
+++ mcclient.py
@@ -20,10 +20,8 @@ OpenGL.FORWARD_COMPATIBLE_ONLY = True
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from ctypes import c_void_p
-from math import cos, sin, radians, ceil
+from math import cos, sin, radians
 
-
-from time import time
 import network.messages as messages
 from network.connection import Connection
 from world import World
@@ -50,165 +48,201 @@ def loadImage(image):
     return texture, width, height
 
 
-if __name__ == '__main__':
-    pygame.init()
-    pygame.display.set_mode((800, 600), pygame.OPENGL | pygame.DOUBLEBUF)
 
-    # VBO
-    vbo = glGenBuffers(1)
-    glBindBuffer(GL_ARRAY_BUFFER, vbo)
+class Player(object):
+    def __init__(self):
+        self.message_pos = None
+        self.speed = [0, 0, 0]
 
-    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
-    glEnable(GL_DEPTH_TEST)
-#    glEnable(GL_ALPHA_TEST)
-    glEnable(GL_BLEND)
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-    glEnable(GL_TEXTURE_2D)
-    glDisable(GL_LIGHTING)
-    glEnableClientState(GL_VERTEX_ARRAY)
-    glEnableClientState(GL_TEXTURE_COORD_ARRAY)
-    glEnableClientState(GL_COLOR_ARRAY)
 
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluPerspective(60, 800./600., 0.1, 128)
 
-#    pygame.event.set_grab(True)
-#    pygame.mouse.set_visible(False)
+class MCClient(object):
+    def __init__(self, name, screen_size, server='localhost', fullscreen=False,
+                 fovy=60, zfar=100):
+        # Set up display
+        pygame.init()
 
-    truc = loadImage('mcdata/terrain.png')
+        flags = pygame.OPENGL | pygame.DOUBLEBUF | pygame.HWSURFACE
+        if fullscreen:
+            flags |= pygame.FULLSCREEN
 
-    world = World()
-    con  = Connection('Bot2')
-    con.connect('localhost')
-    messages.KeepAlive().send(con.socket)
+        self.zfar, self.fovy = zfar, fovy
+        self.width, self.height = screen_size
+        self.aspect_ratio = float(self.width) / float(self.height)
+        self.window = pygame.display.set_mode(screen_size, flags)
 
-    message_pos = None
-    speed_y = 0
+        # Set up GL
+        # 1. Hints and enables/disables: FIXME (programmable pipeline)
+        glEnable(GL_DEPTH_TEST)
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        glEnable(GL_TEXTURE_2D)
+        glDisable(GL_LIGHTING) #TODO
+        glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
+        #TODO: programmable pipeline
+        glEnable(GL_FOG)
+        glFogi(GL_FOG_MODE, GL_LINEAR)
+        # 2. VBO
+        self.vbo = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
+        # 3. ClientStates
+        glEnableClientState(GL_VERTEX_ARRAY)
+        glEnableClientState(GL_TEXTURE_COORD_ARRAY)
+        glEnableClientState(GL_COLOR_ARRAY)
+        # 4. Projections: FIXME (programmable pipeline)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective(fovy, self.aspect_ratio, 0.1, zfar)
 
-    clock = pygame.time.Clock()
+        #TODO: grab mouse?
+        #pygame.event.set_grab(True)
+        #pygame.mouse.set_visible(False)
 
-    quit = False
+        # Load textures
+        terrain = loadImage('mcdata/terrain.png')
 
-    old_time = time()
-    while con.socket and not quit:
-        new_time = time()
-        delta = (new_time - old_time)
-        old_time = new_time
+        # Instantiate world and connection
+        self.world = World()
+        self.connection = Connection(name)
+        self.connection.connect(server)
+        messages.KeepAlive().send(self.connection.socket) #TODO: Ok? Needed?
 
-        msgs = con.get_messages()
-        for message in msgs:
+        self.player = Player()
+
+
+    def handle_messages(self):
+        for message in self.connection.get_messages():
             if type(message) == messages.ChatMessage:
+                #TODO: display on screen
                 print('[MSG] %s' % message.message)
-
-            if type(message) == messages.BlockChange:
-                world.modify_block(message.x, message.y, message.z, message.block_type, message.block_metadata)
-
-            if type(message) == messages.MultiBlockChange:
-                for (ox, oy, oz), block_type, block_metadata in zip(message.coordinates, message.types, message.metadatas):
+            elif type(message) == messages.BlockChange:
+                self.world.modify_block(message.x, message.y, message.z,
+                                        message.block_type,
+                                        message.block_metadata)
+            elif type(message) == messages.MultiBlockChange:
+                for (ox, oy, oz), type_, metadata in zip(message.coordinates,
+                                                         message.types,
+                                                         message.metadatas):
                     ox = ox + message.chunk_x * 16
                     oz = oz + message.chunk_z * 16
-                    world.modify_block(ox, oy, oz, block_type, block_metadata)
-
-            if type(message) == messages.PreChunk:
+                    self.world.modify_block(ox, oy, oz, type_, metadata)
+            elif type(message) == messages.PreChunk:
                 if message.mode == 1:
-                    world.allocate_sector(message.x, message.z)
+                    self.world.allocate_sector(message.x, message.z)
                 else:
-                    world.delete_sector(message.x, message.z)
-
-            if type(message) == messages.MapChunk:
-                world.map_chunk(message.x, message.y, message.z, message.size_x, message.size_y, message.size_z, message.compressed_data)
-
-
-            if type(message) == messages.PlayerPosLook:
+                    self.world.delete_sector(message.x, message.z)
+            elif type(message) == messages.MapChunk:
+                self.world.map_chunk(message.x, message.y, message.z,
+                                     message.size_x, message.size_y,
+                                     message.size_z, message.compressed_data)
+            elif type(message) == messages.PlayerPosLook:
                 print('[PlayerPosLook] %f, %f (%f), %f ; %f, %f'
                         % (message.x, message.y, message.stance, message.z,
                            message.yaw, message.pitch))
-                message_pos = message
-                message.send(con.socket)
-
-            if type(message) == messages.UpdateHealth:
+                self.player.message_pos = message
+                message.send(self.connection.socket)
+            elif type(message) == messages.UpdateHealth:
                 print('[Vie] %d / 20' % message.health)
                 if message.health <= 0:
-                    messages.Respawn(0).send(con.socket)
-
-            if type(message) == messages.Disconnect:
+                    messages.Respawn(0).send(self.connection.socket)
+            elif type(message) == messages.Disconnect:
                 print('[Kicked] %s' % message.reason)
-                quit = True
+                self.running = False
 
 
+    def handle_events(self):
         # Pygame events
         for event in pygame.event.get():
             if event.type == pygame.MOUSEMOTION:
-                if message_pos:
-                    message_pos.yaw += event.rel[0] / 2.
-                    message_pos.pitch += event.rel[1] / 3.
-            if event.type == pygame.KEYDOWN:
+                if self.player.message_pos:
+                    self.player.message_pos.yaw += event.rel[0] / 2.
+                    self.player.message_pos.pitch += event.rel[1] / 3.
+            elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    quit = True
-        # Keyboard
-        key_pressed = pygame.key.get_pressed()
-        # TODO: Move accordingly
-        if key_pressed[pygame.K_z]:
-            #TODO: speed, not pos
-            message_pos.z += cos(radians(message_pos.yaw)) * 4 * delta
-            message_pos.x -= sin(radians(message_pos.yaw)) * 4 * delta
-        if key_pressed[pygame.K_s]:
-            #TODO: speed, not pos
-            message_pos.z -= cos(radians(message_pos.yaw)) * 3 * delta
-            message_pos.x += sin(radians(message_pos.yaw)) * 3 * delta
-        # TODO: jump, if on ground
-        if key_pressed[pygame.K_SPACE] and message_pos.on_ground:
-            speed_y = 8.
+                    self.running = False
+                elif event.key == pygame.K_z:
+                    self.player.speed[2] = 4.
+                elif event.key == pygame.K_s:
+                    self.player.speed[2] = -4.
+                elif event.key == pygame.K_SPACE and self.player.message_pos.on_ground:
+                    self.player.speed[1] = 8.
+            elif event.type == pygame.KEYUP:
+                if event.key in (pygame.K_z, pygame.K_s):
+                    self.player.speed[2] = 0.
 
-        if message_pos:
-            message_pos.y += speed_y * delta
-            message_pos.stance += speed_y * delta
 
-        # TODO: check for collision
-        if message_pos:
-            message_pos.on_ground = True
-            hit_ceiling = world.collide(message_pos)
-            if hit_ceiling:
-                speed_y = min(0, speed_y)
-            if message_pos.on_ground:
-                speed_y = 0
+    def run(self):
+        clock = pygame.time.Clock()
+
+        self.running = True
+        while self.connection.socket and self.running:
+            delta = clock.get_time() / 1000.
+
+            self.handle_messages()
+            self.handle_events()
+
+            if self.player.message_pos:
+                self.player.message_pos.y += self.player.speed[1] * delta
+                self.player.message_pos.stance += self.player.speed[1] * delta
+                self.player.message_pos.z += self.player.speed[2] * cos(radians(self.player.message_pos.yaw)) * delta
+                self.player.message_pos.x -= self.player.speed[2] * sin(radians(self.player.message_pos.yaw)) * delta
+
+            # TODO: check for collision
+            if self.player.message_pos:
+                self.player.message_pos.on_ground = True
+                hit_ceiling = self.world.collide(self.player.message_pos)
+                if hit_ceiling:
+                    self.player.speed[1] = min(0, self.player.speed[1])
+                if self.player.message_pos.on_ground:
+                    self.player.speed[1] = 0
+                else:
+                    self.player.speed[1] = max(self.player.speed[1] - delta * 30, -12.)
+
+            if not self.player.message_pos:
+                messages.KeepAlive().send(self.connection.socket) #TODO: Ok? Needed?
             else:
-                speed_y = max(speed_y - delta * 30, -12.)
+                self.player.message_pos.pitch = max(-90, min(90, self.player.message_pos.pitch))
 
-        if not message_pos:
-            messages.KeepAlive().send(con.socket)
-        else:
-            message_pos.pitch = max(-90, min(90, message_pos.pitch))
+                self.player.message_pos.send(self.connection.socket)
 
-            message_pos.send(con.socket)
+                pos = self.player.message_pos.x, self.player.message_pos.stance, self.player.message_pos.z
 
-            pos = message_pos.x, message_pos.stance, message_pos.z
+                glClearColor(0.2, 0.2, 0.8, 0)
+                glFogfv(GL_FOG_COLOR, (0.2, 0.2, 0.8))
+                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-            glClearColor(0.0, 0.0, 1.0, 0)
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+                #TODO: new pipeline
+                glMatrixMode(GL_MODELVIEW)
+                glLoadIdentity()
+                glRotated(self.player.message_pos.pitch, 1, 0, 0)
+                glRotated(180 + self.player.message_pos.yaw, 0, 1, 0)
+                glTranslated(-self.player.message_pos.x, -self.player.message_pos.stance, -self.player.message_pos.z)
 
-            #TODO: new pipeline
-            glMatrixMode(GL_MODELVIEW)
-            glLoadIdentity()
-            glRotated(message_pos.pitch, 1, 0, 0)
-            glRotated(180 + message_pos.yaw, 0, 1, 0)
-            glTranslated(-message_pos.x, -message_pos.stance, -message_pos.z)
+                nb_vertices = self.world.get_gl_faces(pos, self.fovy,
+                                                      self.aspect_ratio,
+                                                      0.1, self.zfar,
+                                                      self.player.message_pos.yaw,
+                                                      self.player.message_pos.pitch)
+                glFogf(GL_FOG_START, self.zfar * 2 / 3)
+                glFogf(GL_FOG_END, self.zfar)
 
-            nb_vertices = world.get_gl_faces(pos, 60, 800./600., 0.1, 80, message_pos.yaw, message_pos.pitch)
+                glVertexPointer(3, GL_FLOAT, 32, c_void_p(0))
+                glTexCoordPointer(2, GL_FLOAT, 32, c_void_p(12))
+                glColorPointer(3, GL_FLOAT, 32, c_void_p(20))
 
-            glVertexPointer(3, GL_FLOAT, 32, c_void_p(0))
-            glTexCoordPointer(2, GL_FLOAT, 32, c_void_p(12))
-            glColorPointer(3, GL_FLOAT, 32, c_void_p(20))
+                #Uncomment to check backface culling
+                #glPolygonMode(GL_FRONT, GL_POINT)
+                glDrawArrays(GL_QUADS, 0, nb_vertices)
+                glDrawArrays(GL_QUADS, 0, nb_vertices)
 
-#Uncomment to check backface culling
-#            glPolygonMode(GL_FRONT, GL_POINT)
-            glDrawArrays(GL_QUADS, 0, nb_vertices)
-            glDrawArrays(GL_QUADS, 0, nb_vertices)
+            pygame.display.flip()
+            clock.tick(60)
 
-        pygame.display.flip()
-        clock.tick(60)
+        if not self.running:
+            messages.Disconnect().send(self.connection.socket)
 
-    if quit:
-        messages.Disconnect().send(con.socket)
+
+if __name__ == '__main__':
+    client = MCClient('Bot2', (800, 600))
+    client.run()
 
